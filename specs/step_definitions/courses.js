2f9e6779b690d80dc4222f877d88b2e18f24c778
Teddymannen <teddy_kross@hotmail.com>
2023-02-23 11:42:59 +0100

    Update courses.js

--- specs/step_definitions/courses.js
+++ specs/step_definitions/courses.js
@@ -1,108 +1,11 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-/* No duplicate steps, this one already in classes.js
-Given('I am on the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the link with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-Given('I am on the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the div with id {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the li with data-value {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the div with id {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the li with data-value {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the span with data-field {string}', (a) => {});*/
-
-Then('the first row of the tbody with class {string} contains {string} and {string}', (a, b, c) => {
-  // TODO: implement step
+Then('the first row of the tbody with class {string} contains {string} and {string}', (tbodyClass, a, b) => {
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', a);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', b);
 });
 
-/* No duplicate steps, this one already in classes.js
-Given('I am on the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the span with data-field {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the first edit button', () => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-Then('the first row of the tbody with class {string} contains {string} and {string}', (a, b, c) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-Given('I am on the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the span with data-field {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the first edit button', () => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in classes.js
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-Then('the first row of the tbody with class {string} does not contain {string} and {string}', (a, b, c) => {
-  // TODO: implement step
+Then('the first row of the tbody with class {string} does not contain {string} and {string}', (tbodyClass, a, b) => {
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', a);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', b);
 });
