ebb5b4e42810407cc0b29d79cce38355c590d61a
lelonS <35070147+lelonS@users.noreply.github.com>
2023-02-23 11:28:58 +0100

    Update classes.js

--- specs/step_definitions/classes.js
+++ specs/step_definitions/classes.js
@@ -1,120 +1,65 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('I am on the page {string}', (a) => {
-  // TODO: implement step
+Given('I am on the page {string}', (urlEnd) => {
+  // Login
+  cy.visit('/login.html');
+  cy.get('input#email').type('exempel@school.net');
+  cy.get('input#password').type('abc123');
+  cy.get('input[type="submit"]').click();
+
+  // Visit the homepage
+  cy.visit(urlEnd);
+
+  // Wait for the page to load
+  cy.wait(1000);
 });
 
-When('I click on the link with aria-label {string}', (a) => {
-  // TODO: implement step
+When('I click on the link with aria-label {string}', (labelText) => {
+  cy.get('a[aria-label="' + labelText + '"]').click({ force: true });
 });
 
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-Given('I am on the page {string}', (a) => {});*/
-
-When('I fill in input id {string} with {string}', (a, b) => {
-  // TODO: implement step
+When('I fill in input id {string} with {string}', (id, text) => {
+  cy.get('input#' + id).type(text);
 });
 
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-When('I click on the div with id {string}', (a) => {
-  // TODO: implement step
+When('I click on the div with id {string}', (id) => {
+  cy.get('div#' + id).click();
 });
 
-When('I click on the li with data-value {string}', (a) => {
-  // TODO: implement step
+When('I click on the li with data-value {string}', (dataValueText) => {
+  cy.get('li[data-value="' + dataValueText + '"]').click();
 });
 
-/* No duplicate steps, this one already above
-When('I click on the div with id {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the li with data-value {string}', (a) => {});*/
-
-When('I click on the input id {string}', (a) => {
-  // TODO: implement step
+When('I click on the input id {string}', (id) => {
+  cy.get('input#' + id).click();
 });
 
-When('I click on the button with aria-label {string}', (a) => {
-  // TODO: implement step
+When('I click on the button with aria-label {string}', (labelText) => {
+  cy.get('button[aria-label="' + labelText + '"]').click();
 });
 
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-When('I click on the span with data-field {string}', (a) => {
-  // TODO: implement step
+When('I click on the span with data-field {string}', (fieldText) => {
+  cy.get('span[data-field="' + fieldText + '"]').click();
+  cy.wait(1000);
 });
 
-Then('the first row of the tbody with class {string} contains {string}, {string} and {string}', (a, b, c, d) => {
-  // TODO: implement step
+Then('the first row of the tbody with class {string} contains {string}, {string} and {string}', (tbodyClass, a, b, c) => {
+  // firstRow = cy.get('tbody.' + tbodyClass + ' tr').first();
+  // firstRow.should('contain', a);
+  // firstRow.should('contain', b);
+  // firstRow.should('contain', c);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', a);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', b);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', c);
 });
-
-/* No duplicate steps, this one already above
-Given('I am on the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the span with data-field {string}', (a) => {});*/
 
 When('I click on the first edit button', () => {
-  // TODO: implement step
+  // Click on the first <a> with aria-label "Edit"
+  cy.get('a[aria-label="Edit"]').first().click();
 });
 
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I fill in input id {string} with {string}', (a, b) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-Then('the first row of the tbody with class {string} contains {string}, {string} and {string}', (a, b, c, d) => {});*/
-
-/* No duplicate steps, this one already above
-Given('I am on the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the span with data-field {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the first edit button', () => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-/* No duplicate steps, this one already in admin-nav.js
-Then('I get sent to the page {string}', (a) => {});*/
-
-/* No duplicate steps, this one already above
-When('I click on the button with aria-label {string}', (a) => {});*/
-
-Then('the first row of the tbody with class {string} does not contain {string}, {string} and {string}', (a, b, c, d) => {
-  // TODO: implement step
+Then('the first row of the tbody with class {string} does not contain {string}, {string} and {string}', (tbodyClass, a, b, c) => {
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', a);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', b);
+  cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', c);
 });
