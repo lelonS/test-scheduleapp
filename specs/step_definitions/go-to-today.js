1dc8796b382756c7f3b92f9d4ad416eb6a2adc01
Teddymannen <teddy_kross@hotmail.com>
2023-02-23 11:37:14 +0100

    Update go-to-today.js

--- specs/step_definitions/go-to-today.js
+++ specs/step_definitions/go-to-today.js
@@ -1,20 +1,14 @@
 import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
-Given('I am logged in as an admin', () => {
-  // TODO: implement step
-});
-
-/* No duplicate steps, this one already in admin-nav.js
-Given('I am on the homepage', () => {});*/
-
 When('I scroll to the bottom of the page', () => {
-  // TODO: implement step
+  cy.scrollTo('bottom');
 });
 
 When('I click on the button with id {string}', (a) => {
-  // TODO: implement step
+  cy.get(`button#${a}`).click();
 });
 
 Then('the calendar goes back to todays date', () => {
-  // TODO: implement step
+  cy.get('tr[class="today"]')
+    .should('be.visible');
 });
