26f50558b95115bc59f7a0849542f021b9c31c58
Teddymannen <teddy_kross@hotmail.com>
2023-03-01 11:46:56 +0100

    omegalong wait

--- specs/step_definitions/courses.js
+++ specs/step_definitions/courses.js
@@ -2,12 +2,13 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 const waitTime = Cypress.env('waitTimeMs');
 
 Then('the first row of the tbody with class {string} contains {string} and {string}', (tbodyClass, a, b) => {
+  cy.wait(waitTime);
   cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', a);
   cy.get('tbody.' + tbodyClass + ' tr').first().should('contain', b);
 });
 
 Then('the first row of the tbody with class {string} does not contain {string} and {string}', (tbodyClass, a, b) => {
-  // cy.wait(waitTime);
+  cy.wait(waitTime);
   cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', a);
   cy.get('tbody.' + tbodyClass + ' tr').first().should('not.contain', b);
 });
